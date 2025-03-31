bef57f34ef1ac0805703edf3e83d378bb49c9196
kislayrajj <wrkislayraj@gmail.com>
2025-03-31 23:51:24 +0530

    corse fixed

--- socketServer.js
+++ socketServer.js
@@ -1,27 +1,25 @@
-const {Server} = require("socket.io")
+const { Server } = require("socket.io");
 
-const setupSocket = (server)=>{
-const io = new Server(server, {
-    cors:{origin : "http://localhost:5173", method:["GET", "POST"]}
-})
+const setupSocket = (server, allowedOrigins) => {
+  const io = new Server(server, {
+    cors: { origin: allowedOrigins, methods: ["GET", "POST"] },
+  });
 
-io.on("connection", (socket)=>{
-    console.log("A user Connected")
+  io.on("connection", (socket) => {
+    console.log("A user Connected");
 
     socket.on("chat message", (msg) => {
-        console.log("Server received message:", msg);
-        const botEvent = `chat message ${msg.bot}`;
-        socket.broadcast.emit(botEvent, msg); 
+      console.log("Server received message:", msg);
+      const botEvent = `chat message ${msg.bot}`;
+      socket.broadcast.emit(botEvent, msg);
     });
-    
-      
 
-    socket.on("disconnect",()=>{
-        console.log("User Disconnected")
-    })
-})
+    socket.on("disconnect", () => {
+      console.log("User Disconnected");
+    });
+  });
 
-return io
-}
+  return io;
+};
 
-module.exports = setupSocket
+module.exports = setupSocket;
